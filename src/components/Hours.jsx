10ab607d7c1b62749f95b0b22244cd162e84fec9
Kevin Kendle <1066413+KKendle@users.noreply.github.com>
2022-07-07 14:08:59 -0500

    fix indentation from copy+paste

--- src/components/Hours.jsx
+++ src/components/Hours.jsx
@@ -6,34 +6,34 @@ class Hours extends React.Component {
             <section id="hours">
                 <h2>Hours</h2>
                 <ol>
-                <li>
-                    <span className="hours__day">Monday</span>
-                    <span className="hours__time">9am - 2pm</span>
-                </li>
-                <li>
-                    <span className="hours__day">Tuesday</span>
-                    <span className="hours__time">9am - 2pm</span>
-                </li>
-                <li>
-                    <span className="hours__day">Wednesday</span>
-                    <span className="hours__time">9am - 2pm</span>
-                </li>
-                <li>
-                    <span className="hours__day">Thursday</span>
-                    <span className="hours__time">9am - 2pm</span>
-                </li>
-                <li>
-                    <span className="hours__day">Friday</span>
-                    <span className="hours__time">9am - 2pm</span>
-                </li>
-                <li>
-                    <span className="hours__day">Saturday</span>
-                    <span className="hours__time">9am - 2pm</span>
-                </li>
-                <li>
-                    <span className="hours__day">Sunday</span>
-                    <span className="hours__time">9am - 2pm</span>
-                </li>
+                    <li>
+                        <span className="hours__day">Monday</span>
+                        <span className="hours__time">9am - 2pm</span>
+                    </li>
+                    <li>
+                        <span className="hours__day">Tuesday</span>
+                        <span className="hours__time">9am - 2pm</span>
+                    </li>
+                    <li>
+                        <span className="hours__day">Wednesday</span>
+                        <span className="hours__time">9am - 2pm</span>
+                    </li>
+                    <li>
+                        <span className="hours__day">Thursday</span>
+                        <span className="hours__time">9am - 2pm</span>
+                    </li>
+                    <li>
+                        <span className="hours__day">Friday</span>
+                        <span className="hours__time">9am - 2pm</span>
+                    </li>
+                    <li>
+                        <span className="hours__day">Saturday</span>
+                        <span className="hours__time">9am - 2pm</span>
+                    </li>
+                    <li>
+                        <span className="hours__day">Sunday</span>
+                        <span className="hours__time">9am - 2pm</span>
+                    </li>
                 </ol>
             </section>
         )
